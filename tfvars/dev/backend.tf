2976b1f81cf6cf81e94c3df85499db32a1f3841a
Maahi <mahendra48610@gmail.com>
2025-08-07 15:39:06 +0530

    Expense_Ansible

--- tfvars/dev/backend.tf
+++ tfvars/dev/backend.tf
@@ -1,4 +1,8 @@
-bucket = "daws78s-dev"
-key    = "multi-env-tfvars"
-region = "us-east-1"
-dynamodb_table = "daws78s-dev"
+terraform {
+  backend "s3" {
+    bucket         = "daws78s-dev"
+    key            = "multi-env-tfvars"
+    region         = "us-east-1"
+    dynamodb_table = "daws78s-dev"
+  }
+}
--- tfvars/prod/backend.tf
+++ tfvars/prod/backend.tf
@@ -1,4 +1,8 @@
-bucket = "daws78s-prod"
-key    = "multi-env-tfvars"
-region = "us-east-1"
-dynamodb_table = "daws78s-prod"
+terraform {
+  backend "s3" {
+    bucket         = "daws78s-prod"
+    key            = "multi-env-tfvars"
+    region         = "us-east-1"
+    dynamodb_table = "daws78s-prod"
+  }
+}
